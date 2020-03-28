ec39311efe460e16a1f55c384a6ba1cd0fcb2f5c
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-03-28 12:03:24 +0300

    Customize the default error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-app dark>
+  <v-app>
     <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
+      Page not found
     </h1>
     <h1 v-else>
-      {{ otherError }}
+      {{ error.statusCode }} - {{ error.message }}
     </h1>
     <NuxtLink to="/">
       Home page
@@ -21,17 +21,9 @@ export default {
       default: null,
     },
   },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    };
-  },
   head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
-      title,
+      title: this.error.message,
     };
   },
 };
