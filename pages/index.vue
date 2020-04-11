6935a1347be12612b7d770b872bbc7027fce1678
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-12 00:23:30 +0300

    Add logout

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,10 @@
             <span v-else>
               User is not a student! Groups: {{ user.group }}
             </span>
+            <br>
+            <v-btn color="primary" @click="logout">
+              Logout
+            </v-btn>
           </p>
           <p v-else>
             Not logged in!
@@ -51,6 +55,12 @@ import { mapState } from 'vuex';
 export default Vue.extend({
   computed: {
     ...mapState(['user']),
+  },
+  methods: {
+    async logout () {
+      await fetch('/api/v1/auth/logout');
+      window.location.reload();
+    },
   },
 });
 </script>
