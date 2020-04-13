d95c5b2f98a8306bbf9f0702ca6a1d5e959f8e3b
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-13 10:38:46 +0300

    Create dashboard with dummy data

---
+++ components/dashboard/FormCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{ course }} - {{ title }}
+    </v-card-title>
+    <v-card-text>
+      Something
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn>
+        {{ progress == 0 ? 'Start' : 'Continue' }}
+      </v-btn>
+    </v-card-actions>
+    <v-progress-linear bottom :value="progress" />
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    course: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    progress: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+</script>
---
+++ pages/dashboard.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-card tile flat width="100%">
+      <v-card-title class="headline">
+        Available forms
+      </v-card-title>
+      <v-card-text>
+        <v-layout row>
+          <v-flex
+            v-for="form in forms"
+            :key="form.course+form.title"
+            style="padding: 0.5em;"
+            xs12
+            sm6
+            md3
+          >
+            <FormCard v-bind="form" />
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+
+import FormCard from '@/components/dashboard/FormCard';
+
+export default Vue.extend({
+  components: {
+    FormCard,
+  },
+  data () {
+    return {
+      forms: [
+        { course: 'AI', title: 'Week 10', progress: 0 },
+        { course: 'DMDII', title: 'Week 11', progress: 25 },
+        { course: 'AI', title: 'Week 9', progress: 80 },
+      ],
+    };
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+  middleware: ['auth'],
+});
+</script>
