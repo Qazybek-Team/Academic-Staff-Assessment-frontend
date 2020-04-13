2b6655c357bf9afe5547cc4418c82c497d8097ea
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-13 10:37:48 +0300

    Add profile page with Logout button

---
+++ pages/profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn color="primary" @click="logout">
+    Logout
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  methods: {
+    async logout () {
+      await fetch('/api/v1/auth/logout');
+      window.location.href = '/'; // TODO: proper cache cleaning
+    },
+  },
+  middleware: ['auth'],
+});
+</script>
